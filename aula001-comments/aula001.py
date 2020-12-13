bedea390aaea48150391ff167c52e4e8344a0d66
Diogo-Miranda <kit02tempestade@hotmail.com>
2020-12-13 12:38:20 -0300

    aula001, aula002

---
+++ aula001-comments/aula001.py
@@ -0,0 +1,21 @@
+# Isso é um comentário
+# Python é uma linguagem interpretada --> interpretador lê o código e "mostra"
+# !!! Da esquerda para direita, de cima para baixo !!!!
+# A cerquilha é ignorada pelo Python
+
+print('Linha 1')  # Comentário na frente... Dar 2 espaços antes (PEP 8)
+
+# Falando sobre do que se trata essa linha
+print('Linha 2')
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+
+# Comentário em bloco (mais usado para documentação)
+""" 
+ # Tudo entre 3 áspas é comentário em bloco
+"""
+'''
+ # Aqui também é um comentário
+'''
---
+++ aula002-print/aula002.py
@@ -0,0 +1,12 @@
+# Function print
+
+# print(123456)
+# print('Diogo', 'Araujo')  # Por padrão será exibido com espaço
+print('Diogo', 'Araujo', sep='-')  # Utiliza como separador o '-', pelo 'sep'
+# print('João', 'e', 'Maria', sep='-', end='') # 'end' evita quebra de linha
+print('João', 'e', 'Maria', sep='-', end='###')  # 'end' evita quebra de linha, adicionando ####
+print('Lau', 'Zeny', sep='-')
+
+# Imprimir 824.176.070-18
+print("824", "176", "070", sep=".", end='-')
+print("18")
